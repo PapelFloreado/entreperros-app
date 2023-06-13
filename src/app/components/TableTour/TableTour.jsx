a7a57900e79731f899975106dced20f88aca0419
PapelFloreado <101934170+PapelFloreado@users.noreply.github.com>
2023-06-13 17:56:32 -0300

    entreperros v0.04c

--- src/app/components/TableTour/TableTour.jsx
+++ src/app/components/TableTour/TableTour.jsx
@@ -1,18 +1,18 @@
 import Link from "next/link"
 
 const fechas = [
-    /* {
-       
+     {
+        id:1,
         fecha:"11/12/2023",
         lugar:"La Tangente",
         url:"https://www.instagram.com/entreperros_"
     },
     {
-       
+       id: 2,
         fecha:"12/92/1222",
         lugar:"casa",
         url:"https://www.instagram.com/entreperros_"
-    } */
+    }
 ]
 
 const TableTour = () => {
@@ -42,12 +42,12 @@ const TableTour = () => {
                     </thead>
                     {
 
-                    fechas.map((fecha, index)=>(
+                    fechas.map((fecha , index)=>(
+                        <tbody key={index}>
 
                     
-                        <tbody>
                        
-                        <tr key={index} className=" border-b   bg-zinc-800  dark:border-gray-700">
+                        <tr  className=" border-b   bg-zinc-800  dark:border-gray-700">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {fecha.lugar}
                             </th>
