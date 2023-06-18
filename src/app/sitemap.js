1fef972e70c2683d97414e8fe62fd6ec8523fa8b
PapelFloreado <101934170+PapelFloreado@users.noreply.github.com>
2023-06-18 18:45:48 -0300

    entreperros v0.06 robots sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://www.entreperros.com.ar',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://www.entreperros.com.ar/musica',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://www.entreperros.com.ar/galeria',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://www.entreperros.com.ar/bio',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://www.entreperros.com.ar/contacto',
+        lastModified: new Date(),
+      },
+    ]
+  }
